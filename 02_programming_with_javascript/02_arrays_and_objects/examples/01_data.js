eac285870638327970e13502966cbb10467b96ae
ahsanaltafkhan <sp24-bcs-010@isbstudent.comsats.edu.pk>
2026-09-17 12:31:27 +0000

    Add project batch 12

---
+++ 02_programming_with_javascript/02_arrays_and_objects/examples/01_data.js
@@ -0,0 +1,10 @@
+const student = {
+  name: "Ahsan",
+  skills: ["Python", "JavaScript", "Git"],
+  level: "Beginner"
+};
+
+student.skills.push("React");
+
+console.log(student.name);
+console.log(student.skills);
